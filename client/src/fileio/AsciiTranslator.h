ff4be84b4523003be2c8a4b49f077cff2be450a0
Khang Do <khangdminh07@gmail.com>
2026-07-30 18:20:38 +0700

    feat: add file handler and related files to client side via copy

---
+++ client/src/fileio/AsciiTranslator.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+
+class AsciiTranslator {
+public:
+    std::vector<char> encode(const std::vector<char>& raw);
+    std::vector<char> decode(const std::vector<char>& wire);
+
+private:
+    std::vector<char> normalizeToCRLF(const std::vector<char>& in, bool& carryPrevCR);
+    bool encodeCarryCR = false;
+    bool decodeCarryCR = false;
+};
